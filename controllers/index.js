8e01e281be532a6e0802c40ebdb6a4525595ca96
SwathiVinod19 <swathi.vinod@gmail.com>
2023-07-31 19:22:52 -0400

    added code

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 const loginSignOutRoutes = require('./login-signout-routes');
+const facultyProfileRoute = require('./facultyprofileroute'); 
+const newfacultyProfile = require('./newfacultyprofileroute');
+
+
+router.use('/facultygallery', facultyProfileRoute);
+router.use('/newfacultyprofile', newfacultyProfile);
 
 router.use('/', loginSignOutRoutes);
 
